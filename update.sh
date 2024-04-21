27070d1b32cc459c837837a07049e6c42dc395ad
Zoey <zoey@z0ey.de>
2024-04-22 00:48:01 +0200

    Update update.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- update.sh
+++ update.sh
@@ -5,64 +5,18 @@ if [ -z "$TZ" ] || ! echo "$TZ" | grep -q "^[A-Za-z/]\+$"; then
     sleep inf
 fi
 
-if [ -z "$DUK" ] || ! echo "$DUK" | grep -q "^[A-Za-z0-9]\+$"; then
-    echo "DUK is unset or invalid, it can consist of upper letters A-Z, lower letters a-z and numbers 0-9."
+if ! echo "$URL" | grep -q "^https://.*/api/push/.*$"; then
+    echo "URL needs to start with https:// and contain /api/push/."
     sleep inf
 fi
-
-
-if [ "$IPv4" = "false" ] || [ "$(curl -s4 ipv64.net; echo $?)" != "7" ]; then
-    if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
-        echo "IPv4 needs to be true or false."
-        sleep inf
-    fi
-else
-  if [ "$IPv4" != "false" ]; then
-    if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
-      echo "IPv4 needs to be true or false."
-      sleep inf
-    fi
-    echo "IPv4 does not work, disabling it."
-    export IPv4="false"
-  fi
-fi
-
-
-if [ "$IPv6" = "false" ] || [ "$(curl -s6 ipv64.net; echo $?)" != "7" ]; then
-    if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
-        echo "IPv6 needs to be true or false."
-        sleep inf
-    fi
-else
-  if [ "$IPv6" != "false" ]; then
-    if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
-      echo "IPv6 needs to be true or false."
-      sleep inf
-    fi
-    echo "IPv6 does not work, disabling it."
-    export IPv6="false"
-  fi
-fi
-
 
 if ! echo "$UI" | grep -q "^[0-9]\+[smhd]\?$"; then
     echo "UI needs to be a number which can be followed by one of the chars s, m, h or d."
     sleep inf
 fi
 
-if [ "$IPv4" = "false" ] && [ "$IPv6" = "false" ]; then
-    echo "IPv4 and IPv6 disabled, stopping."
-    sleep inf
-fi
-
 
 while true; do
-    if [ "$IPv4" = "true" ]; then
-        curl -sLS4 https://ipv64.net/update.php?key="$DUK" | tee /tmp/IPv4.json
-    fi
-    if [ "$IPv6" = "true" ]; then
-        curl -sLS6 https://ipv64.net/update.php?key="$DUK" | tee /tmp/IPv6.json
-    fi
-
+    curl -sSL "$URL" | tee /tmp/ok.json
     sleep "$UI"
 done
