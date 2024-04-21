5347238ec7ce1191ffe5e42b430db318595744c0
Zoey <zoey@z0ey.de>
2024-04-22 00:50:47 +0200

    Update healthcheck.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-if ! [ -f /tmp/IPv4.json ] && ! [ -f /tmp/IPv6.json ]; then 
+if ! [ -f /tmp/ok.json ]; then 
   exit 1
 fi
 
-if [ -f /tmp/IPv4.json ] && [ "$(jq -r .status </tmp/IPv4.json)" != "success" ]; then 
-  exit 1
-fi
-
-if [ -f /tmp/IPv6.json ] && [ "$(jq -r .status </tmp/IPv6.json)" != "success" ]; then 
+if [ "$(jq -r .ok </tmp/ok.json)" != "true" ]; then 
   exit 1
 fi
 
